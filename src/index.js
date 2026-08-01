19745694d152de03b107edbd47252ab04ffda0af
Tejaswini Samanta <tejaswini.2024.1705@mirandahouse.ac.in>
2026-08-01 21:47:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ function generatePoem(event) {
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a Hindi poem about ${instructionsInput.value}</div>`;
 
   axios.get(apiUrl).then(displayPoem);
 }
