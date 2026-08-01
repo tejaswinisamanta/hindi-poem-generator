45709f67e3025d318f2034b9ceb9706d4810c58c
Tejaswini Samanta <tejaswini.2024.1705@mirandahouse.ac.in>
2026-08-01 17:15:08 +0530

    update html and js files

--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
-function showAnswer(response) {
-  let joke = response.data.answer;
+function displayPoem(event) {
+  
+
   new Typewriter("#joke", {
-    strings: ["Generating a joke for you...please wait", `${joke}`],
+    strings: ["Generating a poem for you...please wait", `${joke}`],
     autoStart: true,
     cursor: "",
-    delay: 10,
+    delay: 1,
   });
-}
 
-function showJoke() {
+
+
+
+  
+}
+function generatePoem() {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "2c00a5b0tfa996f784d39a0b16ee6fo7";
-  let context = "be polite and give short answers";
+  let context = "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML. Make sure to follow user instructions.";
   let prompt =
-    `Write a 4 line hindi poem in hindi about ${}. Do not repeat a poem.`;
+    `User instructions: Generate a Hindi poem about ${instructionsInput.value}. Do not repeat a poem.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  console.log("Processing");
-  axios.get(apiUrl).then(showAnswer);
+ 
+  console.log("Generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
-let button = document.querySelector("button");
-button.addEventListener("click", showJoke);
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem)
